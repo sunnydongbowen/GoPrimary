4fbd95a8c0c89b4d06b0e90fb65d0c396c87d9b0
sunnydongbowen <46588976+sunnydongbowen@users.noreply.github.com>
2023-10-06 07:55:58 +0000

    slice和array的互转

--- slice/arraytoslice_test.go
+++ slice/arraytoslice_test.go
@@ -7,6 +7,9 @@ import (
 
 
 func TestATS(t *testing.T) {
-	
-	
+	array :=[3]int{1,3,4}
+	sl:=array[:]
+	sl[2] +=1
+	fmt.Printf("%v\n",sl)
+
 }
--- slice/slicetoarray_test.go
+++ slice/slicetoarray_test.go
@@ -3,5 +3,25 @@ package slice
 import (
 	"fmt"
 	"testing"
+	"unsafe"
 ) 
+//切片转数组，很少用。知道就好
+func TestSTA(t *testing.T){
+	s:=[]int{1,2,3}
+	var p=(*[3]int)(unsafe.Pointer(&s[0]))
+	p[1] +=10
+	fmt.Printf("%v\n",s)	
+}
+
+func TestSTA2(t *testing.T){
+	s:=[]int{1,2,3}
+	p:=(*[3]int)(s)
+	p[1] +=10
+	fmt.Printf("%v\n",s)
+	// *[3]int,数组指针
+	fmt.Printf("%T\n",p)
+
+
+
+}
 
