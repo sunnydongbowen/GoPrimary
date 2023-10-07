66445dc9c9fe442c9897bf9b192835a7b8df6f0c
sunnydongbowen <46588976+sunnydongbowen@users.noreply.github.com>
2023-10-07 12:40:50 +0000

    封装safemap

--- mutex_geek/mutex1_test.go
+++ mutex_geek/mutex1_test.go
@@ -1,10 +1,37 @@
 package mutexgeek
 
 import (
+	"fmt"
+	"sync"
 	"testing"
 )
 
-func TestXxx(t *testing.T) {
-	
-	
+type safeResource struct{
+	resource map[string]string
+	lock sync.Mutex
 }
+
+func (s *safeResource) Add(key,value string) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	s.resource[key]=value
+}
+
+func TestMu(t *testing.T){
+	var s safeResource
+	//一定要初始化
+	s.resource=map[string]string{}
+	s.Add("key","value")
+	fmt.Println(s.resource)
+}
+
+func TestMu1(t *testing.T) {
+	s:=safeResource{
+		resource: map[string]string{},
+		lock: sync.Mutex{},
+	}
+	fmt.Println(s.resource)	
+	s.Add("name","bowen")
+	fmt.Println(s.resource)
+}
+
